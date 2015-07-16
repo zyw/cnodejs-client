e934223b299ec434fe5e2e02e31f639d9d4df845
zyw <zyw@zywdeMac-mini.local>
2015-07-16 23:33:25 +0800

    添加AppIcon 添加ScrollView 添加TableView

--- cnodejs-client/ViewTopicController.swift
+++ cnodejs-client/ViewTopicController.swift
@@ -9,7 +9,7 @@
 import UIKit
 //import Alamofire
 
-class ViewTopicController: UIViewController,UIWebViewDelegate/*, HttpProtocol*/ {
+class ViewTopicController: UIViewController,UIWebViewDelegate,UITableViewDataSource/*, HttpProtocol*/ {
     
     var topic:JSON?
 //    var topicId:String?
@@ -19,22 +19,26 @@ class ViewTopicController: UIViewController,UIWebViewDelegate/*, HttpProtocol*/ 
     @IBOutlet weak var webView: UIWebView!
  
     @IBOutlet weak var enshrine: UIButton!
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
     //let httpReq = HttpDataSource()
     
     
+    var tableView:UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-//        self.navigationItem.backBarButtonItem?.tintColor = UIColor.whiteColor()
-//        self.navigationController?.navigationItem.leftBarButtonItem?.tintColor = UIColor.whiteColor()
         
         //设置返回按钮的图标
         var bItem = UIBarButtonItem(image: UIImage(named: "iconfont-back"), style: UIBarButtonItemStyle.Plain, target: self, action: "backHeader")
         self.navigationItem.leftBarButtonItem = bItem
         self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
         
+        self.scrollView.indicatorStyle = UIScrollViewIndicatorStyle.White
+        
         initWebView()
+        initTableView()
         
         /*
         httpReq.delegate = self
@@ -46,7 +50,7 @@ class ViewTopicController: UIViewController,UIWebViewDelegate/*, HttpProtocol*/ 
         super.didReceiveMemoryWarning()
     }
     
-    
+    //初始化WebView
     func initWebView(){
         let topicId = self.topic?["id"].string
         var topicContent = self.topic?["content"].string
@@ -59,14 +63,34 @@ class ViewTopicController: UIViewController,UIWebViewDelegate/*, HttpProtocol*/ 
         self.webView.loadHTMLString(titleHtml+topicContent!, baseURL: bundleUrl)
     }
     
+    //初始化TableView
+    func initTableView() -> Void {
+        let screenBounds = UIScreen.mainScreen().bounds
+        self.tableView = UITableView(frame: CGRect(x: 0, y: screenBounds.height, width: screenBounds.width, height: screenBounds.height), style: UITableViewStyle.Plain)
+        self.tableView.dataSource = self
+        
+        self.scrollView.addSubview(self.tableView)
+        
+        let equalWidth = NSLayoutConstraint(item: self.tableView, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: self.scrollView, attribute: NSLayoutAttribute.Width, multiplier: 1, constant: 0)
+        
+        let equalHeight = NSLayoutConstraint(item: self.tableView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: self.scrollView, attribute: NSLayoutAttribute.Height, multiplier: 1, constant: 0)
+        self.tableView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        equalWidth.active = true
+        equalHeight.active = true
+    }
+    
     func backHeader(){
         self.navigationController?.popViewControllerAnimated(true)
     }
-    
-    @IBAction func backIndexList(sender: UIPanGestureRecognizer) {
-        self.navigationController?.popViewControllerAnimated(true)
-    }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
     /*
     // MARK: - Navigation
 
